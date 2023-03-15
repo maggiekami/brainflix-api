f9feaedf00ab78f663f11e368d70f2e95e378659
maggie kaminska <marg.adm.speak@gmail.com>
2023-03-15 18:15:18 +0000

    add post request

--- routes/videos.js
+++ routes/videos.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const fs = require("fs");
-const { v4: uuid } = require("uuid");
+const { v4: uuidv4 } = require("uuid");
 
 const router = express.Router();
 
@@ -30,42 +30,53 @@ router.get("/", (req, res) => {
 });
 
 // just home route = slash / is videos cause use app use - has videos
+
+//   get videos on top, createobject, push, write to file, res.status(201).json(videotobeposted)
+
 router.post("/", (req, res) => {
   const { title, description } = req.body;
   const videos = getAllVideosData();
 
-  // const videoToBePosted = {
-  // "id": uuid(),
-  // "title": title
-  // "channel": "Cool Videos",
-  // "image": "https://i.imgur.com/l2Xfgpl.jpg",
-  // "description": description,
-  // "views": "1,178,023",
-  // "likes": "300,700",
-  // "duration": "3:45",
-  // "video": "https://project-2-api.herokuapp.com/",
-  //   add image instead of video
-  // "timestamp": 1630656720000,
-  // "comments": [
-  //   {
-  //     "id": uuid(),
-  //     "name": "John Smith",
-  //     "comment": "Amazing",
-  //     "likes": 0,
-  //     "timestamp": 1630656720000
-  //   },
-  //   {
-  //     "id": uuid(),
-  //     "name": "Jane Doe",
-  //     "comment": "Great video.",
-  //     "likes": 0,
-  //     "timestamp": 1630656720000
-  //   }]
-  // }
+  const videoToBePosted = {
+    id: uuidv4(),
+    title: title,
+    channel: "Cool Videos",
+    image: "http://localhost:8081/images/image9.jpeg",
+    description: description,
+    views: "1,178,023",
+    likes: "300,700",
+    duration: "3:45",
+    video: "https://project-2-api.herokuapp.com/",
+    // add image instead of video
+    timestamp: 1630656720000,
+    comments: [
+      {
+        id: uuidv4(),
+        name: "John Smith",
+        comment: "Amazing",
+        likes: 0,
+        timestamp: 1630656720000,
+      },
+      {
+        id: uuidv4(),
+        name: "Jane Doe",
+        comment: "Great video.",
+        likes: 0,
+        timestamp: 1630656720000,
+      },
+    ],
+  };
 
   videos.push(videoToBePosted);
-
-  //   get videos on top, createobject, push, write to file, res.status(201).json(videotobeposted)
+  fs.writeFile("./data/videos.json", JSON.stringify(videos), (err) => {
+    if (err) {
+      return res.status(500).json({
+        error: true,
+        message: "Could not write to users JSON file",
+      });
+    }
+    res.status(201).json(videoToBePosted);
+  });
 });
 
 // gives acces to current video (state variable)
